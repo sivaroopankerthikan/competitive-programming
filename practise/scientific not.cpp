612f067b217c646beaf73142453653863ff4a8de
sivaroopankerthikan <sivaroopankerthikan3@gmail.com>
2026-07-07 07:11:21 +0530

    Create scientific not.cpp
    Added my  second code

---
+++ practise/scientific not.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+   using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long double x;
+    cin >> x;
+    x=25 * x * x * x * x * x * x * x* x * x * x * x * x * x * x- 235;
+    cout << scientific << setprecision(2) << x << '\n';
+    return 0;
+}
